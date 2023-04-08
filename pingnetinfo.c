d5f3e22ba9add518c852c34f8550ae4100032ced
Shreya Agrawal <shreya.agrawal2121@gmail.com>
2023-04-08 19:20:25 +0530

    Trace and ping parts almost complete

--- pingnetinfo.c
+++ pingnetinfo.c
@@ -85,24 +85,33 @@ int main(int argc, char *argv[])
     int one = 1, ttl = 1;
     if (setsockopt(sock_icmp, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0)
     {
-        perror("Set options for UDP socket failed\n");
+        perror("Set options for ICMP socket failed\n");
         exit(1);
     }
+    /*
     if (bind(sock_icmp, (struct sockaddr *)&source_addr, sizeof(source_addr)) < 0)
     {
-        perror("Bind for UDP socket failed\n");
+        perror("Bind for ICMP socket failed\n");
         exit(1);
-    }
-
-   // if (bind(sock_icmp, (struct sockaddr *)&source_addr, sizeof(source_addr)) < 0)
-    //{
-       // perror("Bind for ICMP socket failed\n");
-       // exit(1);
-    //}
+    }*/
+    struct sockaddr_in source_now;
+    source_now.sin_addr.s_addr = source_addr.sin_addr.s_addr;
+    source_now.sin_family = AF_INET;
+    source_now.sin_port = htons(20000);
+    printf("Dest IP: %s\n", inet_ntoa(dest_addr.sin_addr));
     int msg = 0;
     while (1)
     {
         int done = 0, timeout = 0;
+        struct timeval tv_out;
+        tv_out.tv_sec = 0;
+        tv_out.tv_usec = TIMEOUT;
+        setsockopt(sock_icmp, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv_out, sizeof(tv_out));
+        struct sockaddr_in from_arr[5];
+        int last_idx = 0;
+        int is_dest = 0;
+        struct sockaddr_in from_addr;
+        int from_len = sizeof(from_addr);
         for (int i = 0; i < 5; i++)
         {
             struct timeval tout;
@@ -110,7 +119,6 @@ int main(int argc, char *argv[])
             tout.tv_sec = 0;
             struct iphdr *iph;
             struct icmphdr *icmph;
-            struct udphdr *udph;
             char datagram[4096] = "\0", *data;
             iph = (struct iphdr *)datagram;
             icmph = (struct icmphdr *)(datagram + sizeof(struct iphdr));
@@ -130,57 +138,128 @@ int main(int argc, char *argv[])
             icmph->un.echo.id = getpid();
             icmph->un.echo.sequence = msg++;
             icmph->code = 0;
-            icmph->checksum = csum((unsigned short *)(datagram + sizeof(struct iphdr)),sizeof(struct icmphdr));
+            icmph->checksum = csum((unsigned short *)(datagram + sizeof(struct iphdr)), sizeof(struct icmphdr));
             if (sendto(sock_icmp, datagram, sizeof(struct iphdr) + sizeof(struct udphdr), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0)
             {
                 perror("Send failed\n");
                 exit(1);
             }
-            //while (1)
-            //{
-                fd_set fd;
-                FD_ZERO(&fd);
-                FD_SET(sock_icmp, &fd);
-                int ret = select(sock_icmp + 1, &fd, NULL, NULL, &tout);
-                if (FD_ISSET(sock_icmp, &fd))
-                {
-                    struct sockaddr_in from_addr;
-                    int from_len = sizeof(from_addr);
-                    char buf[4096];
-                    recvfrom(sock_icmp, buf, sizeof(buf), 0, (struct sockaddr *)&from_addr, &from_len);
-                    struct iphdr *recv_iph;
-                    recv_iph = (struct iphdr *)buf;
-                    if (recv_iph->protocol == IPPROTO_ICMP)
-                    {
-                        icmph = (struct icmphdr *)(buf + sizeof(struct iphdr));
-                        if (icmph->type == ICMP_TIME_EXCEEDED && icmph->code == ICMP_EXC_TTL)
-                        {
-                            printf("Node reached at hop %d with ip %s\n", ttl, inet_ntoa(from_addr.sin_addr));
-                            done = 1;
-                            //break;
-                        }
-                        else if (icmph->type == ICMP_ECHOREPLY && recv_iph->saddr == dest_addr.sin_addr.s_addr)
-                        {
-                            printf("Destination with ip %s reached at hop %d\n", inet_ntoa(from_addr.sin_addr), ttl);
-                            done = 1;
-                            close(sock_icmp);
-                            exit(0);
-                        }
-                        else{
-                           // break;
-                        }
-                    }
-                    
-                   
-                }
-               
-                
-            //}
-            if (done == 1)
+
+            char buf[4096];
+            recvfrom(sock_icmp, buf, sizeof(buf), 0, (struct sockaddr *)&from_addr, &from_len);
+            struct iphdr *recv_iph;
+            recv_iph = (struct iphdr *)buf;
+            if (recv_iph->protocol == IPPROTO_ICMP)
             {
-                break;
+                icmph = (struct icmphdr *)(buf + sizeof(struct iphdr));
+                if (icmph->type == ICMP_TIME_EXCEEDED && icmph->code == ICMP_EXC_TTL)
+                {
+                    from_arr[last_idx++] = from_addr;
+                }
+                else if (icmph->type == ICMP_ECHOREPLY && recv_iph->saddr == dest_addr.sin_addr.s_addr)
+                {
+                    from_arr[last_idx++] = from_addr;
+                    is_dest = 1;
+                }
+                else
+                {
+                }
             }
         }
+        struct sockaddr_in next_node;
+        int max_occur_idx = -1, max_occur = 0;
+        for (int j = 0; j < last_idx; j++)
+        {
+            int occur = 0;
+            for (int k = 0; k < last_idx; k++)
+            {
+                if (from_arr[k].sin_addr.s_addr == from_arr[j].sin_addr.s_addr)
+                {
+                    occur++;
+                }
+            }
+            if (occur > max_occur)
+            {
+                max_occur = occur;
+                max_occur_idx = j;
+            }
+        }
+        if (max_occur_idx != -1)
+        {
+            if (is_dest && from_arr[max_occur_idx].sin_addr.s_addr == dest_addr.sin_addr.s_addr)
+            {
+                printf("Destination reached at hop %d with ip %s\n", ttl, inet_ntoa(from_arr[max_occur_idx].sin_addr));
+                from_addr = from_arr[max_occur_idx];
+                exit(0);
+            }
+            else
+            {
+                printf("Node reached at hop %d with ip %s\n", ttl, inet_ntoa(from_arr[max_occur_idx].sin_addr));
+                from_addr = from_arr[max_occur_idx];
+            }
+        }
+        tv_out.tv_sec = T;
+        tv_out.tv_usec = 0;
+
+        setsockopt(sock_icmp, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv_out, sizeof(tv_out));
+        for (int sz = 0; sz < 1; sz++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                char datagram[4096] = "\0";
+                struct iphdr *ping_iph;
+                struct icmphdr *ping_icmph;
+                ping_iph = (struct iphdr *)datagram;
+                ping_icmph = (struct icmphdr *)(datagram + sizeof(struct iphdr));
+                ping_iph->ihl = 5;
+                ping_iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr) + sz);
+                ping_iph->check = 0;
+                ping_iph->daddr = from_addr.sin_addr.s_addr;
+                ping_iph->saddr = source_addr.sin_addr.s_addr;
+                ping_iph->ttl = ttl;
+                ping_iph->frag_off = 0;
+                ping_iph->id = 10000;
+                ping_iph->tos = 0;
+                ping_iph->version = 4;
+                ping_iph->protocol = IPPROTO_ICMP;
+                ping_iph->check = csum((unsigned short *)datagram, sizeof(struct iphdr));
+                ping_icmph->type = ICMP_ECHO;
+                ping_icmph->un.echo.id = getpid();
+                ping_icmph->un.echo.sequence = msg++;
+                ping_icmph->code = 0;
+                ping_icmph->checksum = csum((unsigned short *)(datagram + sizeof(struct iphdr)), sizeof(struct icmphdr));
+                char *data;
+                data = (char *)(datagram + sizeof(struct iphdr) + sizeof(struct icmphdr));
+                if(sz == 0){
+                    strcpy(data,"\0");
+                }
+                else{
+                    char buf[sz];
+                    strcpy(buf,"\0");
+                    for(int idx = 0; idx < sz; idx++)
+                        buf[idx] = rand()%26 + 'a';
+                    strcpy(data,buf);
+                }
+                struct timeval curr_time,recv_time;
+                gettimeofday(&curr_time,NULL);
+                char recv_buf[4096];
+                sendto(sock_icmp,datagram,sizeof(struct iphdr) + sizeof(struct icmphdr) + sz,0,(struct sockaddr *)&from_addr,sizeof(from_addr));
+                struct sockaddr_in from_addr_ping;
+                int fromlen = sizeof(from_addr_ping);
+                recvfrom(sock_icmp,recv_buf,sizeof(recv_buf),0,(struct sockaddr *)&from_addr_ping,&fromlen);
+                struct iphdr *ping_recv_iph;
+                struct icmphdr *ping_recv_icmph;
+                ping_recv_iph = (struct iphdr *)recv_buf;
+                ping_recv_icmph = (struct icmphdr *)(recv_buf + sizeof(struct iphdr));
+                if(ping_recv_iph->protocol == IPPROTO_ICMP){
+                    if(ping_recv_icmph->type == ICMP_ECHOREPLY && ping_recv_iph->saddr == from_addr.sin_addr.s_addr){
+                        gettimeofday(&recv_time,NULL);
+                        printf("Echo reply received on IP: %s\n",inet_ntoa(from_addr.sin_addr));
+                    }
+                }
+            }
+        }
+
         ttl++;
     }
     return 0;
